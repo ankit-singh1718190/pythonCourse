2bf92120d8f0e042fd203270dcc6da4a19083314
Ankit Singh <ankit1718190@gmail.com>
2024-10-19 15:27:32 +0530

    intial

---
+++ funcation_.py
@@ -0,0 +1,68 @@
+# def sum(a,b):
+#     plus=a+b
+#     print(plus)
+#     return sum
+
+# sum(12,23)
+
+# def MyFuncation(a,b):
+#     return a+b
+# a=MyFuncation(1,2)
+# print(a)
+
+# def myFuncaton(a,b,c):
+#     sum=a+b+c
+#     avg=sum/3
+#     print(avg)
+#     return avg
+# myFuncaton(2,3,4)
+
+# city=["Lucknow","pune","channi","mumbai"]
+# hero=["ankit","aju"]
+# car=["bmw","oddy","alto"]
+# def MyCitylen(list):
+#     print(len(list))
+
+# def mylist(list):
+#     for i in list:
+#         print(i,end=" ")
+
+# print(hero)
+
+# def fun(Usd_value):
+#     inr_value=Usd_value*83
+#     print(Usd_value,"usd=",inr_value)
+# fun(222)    
+
+# a=int(input("Enter the Number :"))
+# def oddAndEvenN(a):
+#     if a%2==0:
+#         print("number is Even")
+#     else:
+#         print("number is Odd") 
+# oddAndEvenN(a)           
+# def show(n):
+#     if n==0:
+#         return
+#     print(n)
+#     show(n-1)
+# show(5)    
+
+# def Natural(n):
+#     if n==0:
+#         return 0
+#     #print(n)
+#     return Natural(n-1)+n
+# print(Natural(5))    
+# def MyFuncation():
+#     print("i am aNki")
+# MyFuncation()    
+# def MyFuncation(a,b):
+#     c=a+b
+#     print(c)
+# MyFuncation(1,2)
+def myFuncation(a,b):
+    c=a+b
+    return c
+out=myFuncation(1,2)
+print(out)
